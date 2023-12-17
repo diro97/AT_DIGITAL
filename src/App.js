248d080b9f8d5749ce16b2df683fab68d8da0098
unknown <loganathandiroshan.com>
2023-12-17 15:46:42 +0530

    at

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./pages/Footer";
+import TopNavigation from "./pages/TopNavigation";
+import Bodypart from "./pages/Bodypart";
+
+function App(){
+  return (
+    <div className="App">
+      <Footer/>
+      <TopNavigation/>
+      <Bodypart/>
+      
+    </div>
+  );
+}
+
+export default App;
